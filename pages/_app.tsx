e5554e39a860585782d80a9d22a7174a3b8c4da6
Mehmet Faruk Koç <mfarukkoc@gmail.com>
2021-12-29 13:13:20 +0300

    Add Layout & CharacterList provider to the App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import CharacterListProvider from "components/character-list-context/CharacterListContext";
+import Layout from "components/layout/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Rick and Morty</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CharacterListProvider>
+        <Component {...pageProps} />
+      </CharacterListProvider>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
