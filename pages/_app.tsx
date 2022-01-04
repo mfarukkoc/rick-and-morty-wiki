f3587ffc5b80ae06363e1ebff9c4fd0461933f14
Mehmet Faruk Koç <mfarukkoc@gmail.com>
2022-01-04 13:38:19 +0300

    Add loading progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import "../styles/globals.css";
+import "styles/globals.scss";
 import type { AppProps } from "next/app";
 import CharacterListProvider from "components/character-list-context/CharacterListContext";
 import Layout from "components/layout/Layout";
 import Head from "next/head";
+
+import Router from "next/router";
+import nProgress from "nprogress";
+import "styles/nprogress.scss";
+
+nProgress.configure({
+  showSpinner: false
+});
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
